1fd1f02f73cd847d80cf51c35e7862f3a1dbc854
lehaneya <lehaneya@gmail.com>
2026-04-08 21:46:53 -0400

    testing the webhoook

--- aaron-bad-file.js
+++ aaron-bad-file.js
@@ -3,14 +3,12 @@ const sqlite3 = require('sqlite3').verbose();
 const AWS = require('aws-sdk');
 const app = express();
 
-// --- VULNERABILITY: HARDCODED SECRETS ---
-// In a real scenario, actual AWS Access Keys would be placed here.
-// Storing credentials directly in source code is a major security risk.
 const aws_config = {
-  accessKeyId: "REPLACED_WITH_DUMMY_KEY_FOR_TESTING",
-  secretAccessKey: "REPLACED_WITH_DUMMY_SECRET_FOR_TESTING",
+  accessKeyId: "AKIAIOSFODNN7EXAMPLE", 
+  secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
   region: "us-east-1"
 };
+
 const s3 = new AWS.S3(aws_config);
 
 const db = new sqlite3.Database(':memory:');
@@ -20,8 +18,6 @@ app.use(express.json());
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // VULNERABLE: Direct concatenation of user input into the SQL string.
-  // This allows an attacker to manipulate the query logic.
   const query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
 
   db.all(query, (err, rows) => {
